92f18604492c394894b20eebabf48f6f2f365b82
SiHE-master <32930517+SiHE-master@users.noreply.github.com>
2021-01-15 10:52:39 +0800

    Add files via upload

--- ctn.sh
+++ ctn.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 #ctn.sh
-nohup python3 ctn.py &
+PID=$(ps -ef|grep ctn.py|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
+if [ "$PID" = "" ]
+then
+  nohup python3 ctn.py &
+else
+  kill -9 $(ps -ef|grep ctn.py|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
+  nohup python3 ctn.py &
+fi
